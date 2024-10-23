dc6378926d7dd75bc327091af90f636d45e0ad11
sujal kumar <sujalkumar0444@gmail.com>
2024-10-23 14:45:01 +0530

    lets ee

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,19 +2,19 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBgR_jwf-XcOFG5BcTb1vjzEKXH1yQbON4",
-    authDomain: "deep-c172c.firebaseapp.com",
-    projectId: "deep-c172c",
-    storageBucket: "deep-c172c.appspot.com",
-    messagingSenderId: "298440114658",
-    appId: "1:298440114658:web:5d9c3f672d92e6df9839bb",
-    measurementId: "G-QF1K2LZ3LC"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBgR_jwf-XcOFG5BcTb1vjzEKXH1yQbON4",
+//     authDomain: "deep-c172c.firebaseapp.com",
+//     projectId: "deep-c172c",
+//     storageBucket: "deep-c172c.appspot.com",
+//     messagingSenderId: "298440114658",
+//     appId: "1:298440114658:web:5d9c3f672d92e6df9839bb",
+//     measurementId: "G-QF1K2LZ3LC"
+//   };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
 
 // messaging.onBackgroundMessage((payload) => {
 //     console.log('Received background message ', payload);
