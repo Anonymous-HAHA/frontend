bc46dab297181211ce9b929c9f81760d9f169262
sujal kumar <sujalkumar0444@gmail.com>
2024-10-23 15:10:47 +0530

    lets ee

--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,10 @@ import { initializeApp } from 'firebase/app';
 import { getMessaging, getToken } from 'firebase/messaging';
 
 // console.log(process.env);
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
-// console.log(firebaseConfig);
-
-
-// Initialize Firebase
+const crypto = require('crypto-js');
+const encryptedContent = process.env.REACT_APP_CONFIG_TEXT;
+const bytes = crypto.AES.decrypt(encryptedContent, process.env.REACT_APP_FIREBASE_SECRET_KEY);
+const firebaseConfig = JSON.parse(bytes.toString(crypto.enc.Utf8));
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Messaging
