1af1f30cf74acd7824b52546cacbedd2fbd7443e
sujal kumar <sujalkumar0444@gmail.com>
2024-10-24 19:10:38 +0530

    done

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,15 @@
 // public/firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
+import { onBackgroundMessage } from "firebase/messaging/sw";
+
+onBackgroundMessage(messaging, (payload) => {
+  console.log('Background message received: ', payload);
+  const { title, body } = payload.notification;
+  self.registration.showNotification(title, {
+    body,
+    icon: 'https://frontend-ten-pi-46.vercel.app/deep.jpeg', // Add a custom icon if needed
+  });
+});
 
